c28f400946821d43bef97f50f002fdf61a29c95b
ali025 <aliby25@gmail.com>
2024-02-24 14:21:47 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# mypackage
+
+mypackage is a Python library for testing.
+
+## Installation
+
+Use the package manager [pip](https://github.com/ali025/mypackage.git) to install foobar.
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='EDSA example python package',
+    long_description=open('README.md').read(),
+    install_requires=['numpy'],
+    url='https://github.com/ali025/mypackage',
+    author='ali',
+    author_email='aliby25@gmail.com'
+)
